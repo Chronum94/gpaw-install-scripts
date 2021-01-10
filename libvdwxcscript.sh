192215d0a76faf91a4108f11969b8f67257ef06f
Anubhab Haldar <ahaldar@bu.edu>
2021-01-10 14:01:54 -0500

    Create libvdwxcscript.sh

---
+++ libvdwxcscript.sh
@@ -0,0 +1,11 @@
+GPAW_INSTALL_ROOT=$(pwd)
+FFTW3_INSTALL_DIR=$GPAW_INSTALL_ROOT/fftw-install/
+
+git clone https://gitlab.com/libvdwxc/libvdwxc.git
+cd libvdwxc
+autoreconf -i
+mkdir build
+cd build
+
+MPI_LIBS="-L/$MPILIBDIR -lmpi" ^C=mpicc FC=mpif90 CFLAGS="-mavx -mtune=native -O3 -ftree-vectorize" FFTW3_INCLUDES="-I$FFTW_INSTALL_DIR/include" FFTW3_LIBS="-L$FFTW_INSTALL_DIR/lib -lfftw3 -lfftw3_mpi" ../configure --prefix=$LIBVDWXC_INSTALL_DIR
+make -j
