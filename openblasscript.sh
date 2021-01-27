4d6ec6ab9121cd5c23ce1884b02f45ee9b4aa774
Anubhab Haldar <ahaldar@bu.edu>
2021-01-27 12:27:45 -0500

    Update openblasscript.sh

--- openblasscript.sh
+++ openblasscript.sh
@@ -4,5 +4,5 @@ OPENBLAS_INSTALL_DIR=$GPAW_INSTALL_ROOT/openblas-install
 git clone https://github.com/xianyi/OpenBLAS.git
 cd OpenBLAS
 git checkout v0.3.9
-make TARGET=ZEN
+make TARGET=ZEN DYNAMIC_ARCH=1
 make PREFIX=$OPENBLAS_INSTALL_DIR install
